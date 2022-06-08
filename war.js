843eee1cc0272f34a60ec900855403a298664dfa
apple <apple@apples-MacBook-Pro.local>
2022-06-08 10:28:15 -0400

    Finished game, now checking for bugs

--- war.js
+++ war.js
@@ -44,7 +44,8 @@ class Player{
 
     draw()
     {
-        let card = deck.pop()
+        let card = this.deck.pop();
+        console.log(card)
         console.log(`${this.name} drew a ${card.rank} of ${card.symbol}`) 
         return card;
     }
@@ -52,7 +53,8 @@ class Player{
     warDraw()
     {
         let cards = this.deck.splice(this.deck.length - 4);
-        console.log(`${this.name}'s fourth card: ${card.rank} of ${card.symbol}`);
+        console.log(cards)
+        console.log(`${this.name}'s fourth card: ${cards[3].rank} of ${cards[3].symbol}`);
         return cards;
     }
 }
@@ -63,19 +65,32 @@ class Game
     {
         this.player1 = player1;
         this.player2 = player2;
+        this.winner;
     }
 
+    /**
+    Start the game
+    */
     startGame()
     {
-        while(this.player1.deck.length > 0 || this.player2.deck.length > 0)
+        let roundNumber = 1;
+        while(this.winner === undefined && this.player1.deck.length > 0 && this.player2.deck.length > 0)
         {
+            console.log("----------------------------------------")
+            console.log(`Round Number: ${roundNumber}`)
+            console.log(`Player 1 Deck Length: ${this.player1.deck.length}`)
+            console.log(`Player 2 Deck Length: ${this.player2.deck.length}`)
             this.startRound();
+            roundNumber++;
         }
-        if(this.player1.deck.length <= 0)
+        if(this.winner !== undefined)
+            console.log(`${this.winner.name} has won the game!`)    
+        else if(this.player1.deck.length <= 0)
             console.log(`${this.player1.name} has won the game!`)
-        else
+        else if(this.player2.deck.length <=0)
             console.log(`${this.player2.name} has won the game!`)
-
+        console.log("-------------------------------------")
+        console.log("")
     }
 
     startRound()
@@ -83,39 +98,70 @@ class Game
         let p1Card = this.player1.draw();
         let p2Card = this.player2.draw();
         
+        let p1Deck = this.player1.deck;
+        let p2Deck = this.player2.deck;
+
         if(p1Card.score === p2Card.score)
         {
             console.log("WAR!")
-            this.startWar()
+            if(p1Deck.length >= 4 && p2Deck.length >= 4)
+                this.startWar(p1Card, p2Card)
+            else{
+                if(p1Deck.length < 4)
+                    this.winner = this.player2;
+                else
+                    this.winner = this.player1
+            }
         }
         else if(p1Card.score > p2Card.score)
         {
             console.log(`Winner: ${this.player1.name} Card Played: ${p1Card.rank} | ${p1Card.symbol}`)
-            this.p1.deck.push(p1Card, p2Card)
+            p1Deck.unshift(p1Card, p2Card)
         }else{
             console.log(`Winner: ${this.player2.name} Card Played: ${p2Card.rank} | ${p2Card.symbol}`)
-            this.p2.deck.push(p2Card, p1Card)
+            p2Deck.unshift(p2Card, p1Card)
         }
+        console.log("---------------------------------------")
+        console.log("")
     }
 
-    startWar()
+    startWar(card1, card2)
     {
         let player1FourCards = this.player1.warDraw();
         let player2FourCards = this.player2.warDraw();
-        let prizeCards = [...player1FourCards, ...player2FourCards]
-
-        while(player1FourCards[3].score === player2FourCards[3].score)
+        let prizeCards = [card1, card2, ...player1FourCards, ...player2FourCards]
+        console.log("Prize Cards:")
+        console.log(prizeCards)
+        while(this.winner === undefined && (player1FourCards[3].score === player2FourCards[3].score))
         {
-            player1FourCards = this.player1.warDraw();
-            player2FourCards = this.player2.warDraw();
-            prizeCards.push(...player1FourCards, ...player2FourCards)
+            if(this.player1.length >= 4 && this.player2.length >= 4)
+            {
+                console.log(`Prize Cards: ${prizeCards}`)
+                player1FourCards = this.player1.warDraw();
+                player2FourCards = this.player2.warDraw();
+                console.log(player1FourCards)
+                console.log(player2FourCards)
+                prizeCards = [...prizeCards, ...player1FourCards, ...player2FourCards]
+            }else{
+                if(this.player1.length >= 4)
+                    this.winner = this.player1
+                else
+                    this.winner = this.player2
+            }
         }
 
-        if(player1FourCards[3].score > player2FourCards[3].score)
+        if(this.winner === undefined)
         {
-            player1.deck.push(prizeCards)
-        }else{
-            player2.deck.push(prizeCards)
+            if(player1FourCards[3].score > player2FourCards[3].score)
+            {
+                this.player1.deck = [...prizeCards, ...this.player1.deck]
+                console.log("Player 1 Deck:")
+                console.log(this.player1.deck)
+            }else{
+                this.player2.deck = [...prizeCards,...this.player2.deck]
+                console.log("Player 2 Deck:")
+                console.log(this.player2.deck)
+            }
         }
     }
 }
@@ -143,3 +189,6 @@ console.log("Player 1's deck")
 console.log(player1.deck)
 console.log("Player 2's deck")
 console.log(player2.deck)
+
+let game = new Game(player1, player2)
+game.startGame();
