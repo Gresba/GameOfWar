b2ef14bbc0ea53b2a250a63466454825de884518
apple <apple@apples-MacBook-Pro.local>
2022-06-09 13:42:30 -0400

    Modified code to be more modular

--- war.js
+++ war.js
@@ -71,7 +71,7 @@ class Deck {
    */
   handOut(player) {
     // Picks a random 
-    player.deck.push(this.deck.splice(Math.floor((Math.random() * this.deck.length)), 1)[0])
+    player.deck.push(this.deck.splice(Math.floor((Math.random() * this.deck.length)), 1)[0]);
   }
 }
 
@@ -97,6 +97,7 @@ class Player {
     // Setting the properties
     this.name = name;
     this.deck = [];
+    this.drawnCard;
   }
 
   /**
@@ -104,11 +105,8 @@ class Player {
    */
   draw() {
     // Get the card at the top
-    let card = this.deck.pop();
-
-    // Show the card
-    console.log(`${this.name} drew a ${card.rank} of ${card.symbol}`)
-    return card;
+    this.drawnCard = this.deck.pop();
+    console.log(`${this.name} drew a ${this.drawnCard.rank} of ${this.drawnCard.symbol}`)
   }
 
   /**
@@ -129,51 +127,60 @@ class Player {
  * Game class which stores all information and actions for starting and finishing a game
  * 
  * Properties:
+ * - currentRound: The current round the game is on
  * - player1: The first player of the game
  * - player2: The second player of the game
  * - winner: The winner of the game
+ * - loser: The loser of the game
  *
  * Functions:
- * - startGame(): Start the game and play until there is a winner
- * - startRound(): Start a round and print out information about the winner
+ * - startGame(): Start the game and play until there is a winner.
+ * - startRound(): Start a round and print out information about the winner.
  * - startWar(): Start a war between two players when they draw the same card. Pass in the cards to store them.
+ * - getRoundWinner(): Return the winner and the loser of a round in an array.
  */
 class Game {
+
   /**
-   * Declare and initialize the properties of the Game class
+   * Declare and initialize the properties of the Game class.
    *
-   * @param player1 The first player
-   * @param player2 The second player
+   * @param {number} currentRound The current round the game is on
+   * @param {Player} player1 The first player
+   * @param {Player} player2 The second player
    */
   constructor(player1, player2) {
-    // Set the players
+    // Set the game's properties
+    this.currentRound = 1;
     this.player1 = player1;
     this.player2 = player2;
     this.winner;
+    this.loser;
   }
 
   /**
    * Start the game and stop when there is a winner (Either a winner is declared or a player runs out of cards)
    */
   startGame() {
-    // Counter to indicate the round number
-    let roundNumber = 1;
 
-    // Condition for the game to keep going
+    /**
+     * Condition for the game to keep going
+     * Winner is not set or both players have cards.
+     */
     while (this.winner === undefined && this.player1.deck.length > 0 && this.player2.deck.length > 0) {
       // Prints out information about a round and the player's state
       console.log("----------------------------------------")
-      console.log(`Round Number: ${roundNumber}`)
+      console.log(`Round Number: ${this.currentRound}`)
       console.log(`Player 1 Deck Length: ${this.player1.deck.length}`)
       console.log(`Player 2 Deck Length: ${this.player2.deck.length}`)
       this.startRound();
-      roundNumber++;
+
+      // Starting another round
+      this.currentRound++;
     }
 
     // Game has ended now figure out why
-    if (this.winner) {
+    if (this.winner)
       console.log(`${this.winner.name} has won the game!`)
-    }
     else if (this.player1.deck.length <= 0)
       console.log(`${this.player2.name} has won the game!`)
     else if (this.player2.deck.length <= 0)
@@ -183,55 +190,58 @@ class Game {
   }
 
   /**
-   * Start a round by taking a card from each players hand and comparing it
+   * Start a round by taking a card from each players hand and comparing them
    */
   startRound() {
-    // Take a card from each players deck  
-    let p1Card = this.player1.draw();
-    let p2Card = this.player2.draw();
+    // Take a card from each players deck 
+    let player1 = this.player1;
+    let player2 = this.player2;
+
+    player1.draw();
+    player2.draw();
 
     // Store deck in variables for simplicity  
     let p1Deck = this.player1.deck;
     let p2Deck = this.player2.deck;
 
     // Condition to start a war  
-    if (p1Card.score === p2Card.score) {
+    if (player1.drawnCard.score === player2.drawnCard.score) {
       console.log("WAR!")
 
       // Check if both players have enough cards to go into war  
       if (p1Deck.length >= 4 && p2Deck.length >= 4)
-        this.startWar(p1Card, p2Card);
+        this.startWar(player1.drawnCard, player2.drawnCard);
       else {
-        
+
         // Figure out which player doesn't have enough cards and declare the winner  
         if (p1Deck.length < 4) {
-          console.log(`${this.player1.name} did not have enough cards to start a war so he lost!`);
-          this.player2.deck.unshift(...this.player1.deck, p1Card, p2Card);
-          this.winner = this.player2;
-          this.player1.deck = [];
+          this.winner = player2;
+          this.loser = player1;
         } else {
-          console.log(`${this.player2.name} did not have enough cards to start a war so he lost!`);
-          this.winner = this.player1;
-          this.player1.deck.unshift(...this.player2.deck, p2Card, p1Card);
-          this.player2.deck = [];
+          this.winner = player1;
+          this.loser = player2;
         }
+        console.log(`${this.loser.name} did not have enough cards to start a war so he lost!`);
+
+        // Add all the cards into the winner's deck
+        this.winner.deck.unshift(...this.loser.deck, this.loser.drawnCard, this.winner.drawnCard);
+
+        // Clear the loser's deck
+        this.loser.deck = [];
       }
     }
 
     // Normal round. Figure out the winner, then give the winner the cards
     else {
-        let roundWinner = this.getRoundWinner(p1Card, p2Card);
-        console.log(`Winner: ${roundWinner.name}`);
-        if(roundWinner === this.player1)
-            roundWinner.deck.unshift(p1Card, p2Card)
-        else
-            roundWinner.deck.unshift(p2Card, p1Card)
+      let roundWinner = this.getRoundWinner(player1.drawnCard, player2.drawnCard);
+      console.log(`Winner: ${roundWinner[0].name}`);
+      roundWinner[0].deck.unshift(roundWinner[0].drawnCard, roundWinner[1].drawnCard);
     }
 
     // Print out the information about the players' decks  
     console.log("")
-    console.log(`${this.player1.name} has ${this.player1.deck.length} cards left`)
-    console.log(`${this.player2.name} has ${this.player2.deck.length} cards left`)
+    console.log(`${player1.name} has ${player1.deck.length} cards left`)
+    console.log(`${player2.name} has ${player2.deck.length} cards left`)
     console.log("---------------------------------------")
     console.log("")
   }
@@ -239,55 +249,53 @@ class Game {
   /**
    * Start a war and pass the cards to the winning player
    * Figure out if a player also does not have enough cards
-   *
-   * @param {Card} card1 Player 1's original card
-   * @param {Card} card2 Player 2's original card
    */
-  startWar(card1, card2) {
-    
+  startWar() {
+
     // Get four cards from each players decks  
     let player1FourCards = this.player1.warDraw();
     let player2FourCards = this.player2.warDraw();
 
     // Store all card in a prize array  
-    let prizeCards = [card1, card2, ...player1FourCards, ...player2FourCards]
+    let prizeCards = [player1.drawnCard, player2.drawnCard, ...player1FourCards, ...player2FourCards]
 
     // Check if there is a winner to the war  
     while (this.winner === undefined && (player1FourCards[3].score === player2FourCards[3].score)) {
       console.log("ANOTHER WAR")
-      
+
       // Check if both players have enough cards to start the war  
       if (this.player1.deck.length >= 4 && this.player2.deck.length >= 4) {
         player1FourCards = this.player1.warDraw();
         player2FourCards = this.player2.warDraw();
         prizeCards.push(...player1FourCards, ...player2FourCards);
 
-      /**
-       * If one of the players don't have enough cards then figure out which player.    
-       * Give the winner all the cards in the game.
-       * Clear the losing player's deck.
-       */
+        /**
+         * If one of the players don't have enough cards then figure out the player.
+         * Set the winner and loser
+         */
       } else {
         if (this.player1.deck.length >= 4) {
-          console.log(`${this.player2.name} didn't have enough cards and lost. All cards have been given to the other player`)
-          this.winner = this.player1
-          this.player1.deck.unshift(...this.player2.deck, ...prizeCards)
-          this.player2.deck = [];
+          this.winner = this.player1;
+          this.loser = this.player2;
+        } else {
+          this.winner = this.player2;
+          this.loser = this.player1;
         }
-        else {
-          console.log(`${this.player1.name} didn't have enough cards and last. All cards have been given to the other player`)
-          this.winner = this.player2
-          this.player2.deck.unshift(...this.player1.deck, ...prizeCards)
-          this.player1.deck = [];
-        }
+
+        console.log(`${this.loser.name} didn't have enough cards and lost. All cards given to the winner!`)
+
+        // Give the winner all the cards
+        this.winner.deck.unshift(...this.loser.deck, ...prizeCards);
+
+        // Erase the losers deck
+        this.loser.deck = [];
       }
     }
-   
+
     // If there is not a winner, get the player with the higher fourth card and add all the cards to their deck   
-    if(!this.winner)
-    {
-        let roundWinner = this.getRoundWinner(player1FourCards[3], player2FourCards[3]);
-        roundWinner.deck.unshift(...prizeCards);  
+    if (!this.winner) {
+      let roundWinner = this.getRoundWinner(player1FourCards[3], player2FourCards[3]);
+      roundWinner[0].deck.unshift(...prizeCards);
     }
   }
 
@@ -296,17 +304,16 @@ class Game {
    * 
    * @param {Card} card1 Player 1's card
    * @param {Card} card2 Player 2's card
-   * @returns {Player} The round winning player
+   * @returns {[Player, Player]} The first index is the winning player. The second index is the losing player.
    */
-  getRoundWinner(card1, card2)
-  {
-      let winningCard;
-      
-      // Compare the scores and return winner
-      if(card1.score > card2.score)
-          return this.player1;
-      else
-          return this.player2;
+  getRoundWinner(card1, card2) {
+    let winningCard;
+
+    // Compare the scores and return winner
+    if (card1.score > card2.score)
+      return [this.player1, this.player2];
+    else
+      return [this.player2, this.player1];
   }
 }
 
@@ -327,7 +334,8 @@ for (let i = 0; i < numberOfCards; i++) {
   else
     deck.handOut(player2)
 }
-
+console.log(player1.deck)
+console.log(player2.deck)
 // Start the game between player1 and player2
 let game = new Game(player1, player2)
 game.startGame();
